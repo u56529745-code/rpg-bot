06f6187c4b062fa78f933195c6181658d93356cc
u56529745-code <u56529745@gmail.com>
2026-09-22 13:52:41 +0300

    Update rpg_bot.py

--- rpg_bot.py
+++ rpg_bot.py
@@ -7,7 +7,7 @@ from db import init_db, get_player, save_player, exp_needed, prof_level_for_exp
 from battle import calc_player_stats, make_mob, player_turn, mob_turn, roll_herb, roll_ore, battle_text
 from craft import can_craft, do_craft
 
-TOKEN = "8620344298:AAFEzGchcbTgDtUx4UtM7-LPSx5oamVdIoY"
+TOKEN = "8620344298:AAGkzQDkRAFKysqtQ7HtmYyWkRAuHSmTxTY"
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
