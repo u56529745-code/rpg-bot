419323fd89d1d47d6f5b70f3fe4056e6a3a111d3
u56529745-code <u56529745@gmail.com>
2026-09-23 10:55:13 +0300

    Update rpg_bot.py

--- rpg_bot.py
+++ rpg_bot.py
@@ -10,7 +10,7 @@ from battle import (
 )
 from craft import can_craft, do_craft, can_craft_void, do_craft_void
 
-TOKEN = "8620344298:AAHr_PhXczz08rhQHgd_DpQt13rwIA2XPgA"
+TOKEN = "8620344298:AAEYen6iM1Kjb4Gm4U3yHWGhEk7r2hG57k8"
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
